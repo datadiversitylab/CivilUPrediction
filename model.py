cc9c3a0635886eefb57a0036174aad30e04c2b37
Danielle Van Boxel <vanboxel@math.arizona.edu>
2024-05-03 12:46:29 -0700

    Basic model tested and viz made

--- model.py
+++ model.py
@@ -11,6 +11,9 @@ from sklearn.model_selection import train_test_split
 import sklearn.ensemble
 import sklearn.metrics
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+plt.ion()
 
 import warnings
 warnings.filterwarnings('ignore')
@@ -42,6 +45,9 @@ if __name__ == '__main__':
     all_summary = []
     target_rasts = dict()
     pred_rasts = dict()
+    models = dict()
+    x_save_test = dict()
+    y_save_test = dict()
 
     for year in years:
         dfiles = year_data[year]
@@ -66,9 +72,13 @@ if __name__ == '__main__':
         # TODO: be careful here, really want the same split every year
         xtrain, xtest, ytrain, ytest = train_test_split(x, y, random_state=seed)
 
+        x_save_test[year] = xtest
+        y_save_test[year] = ytest
+
         np.random.seed(seed)
         model = sklearn.ensemble.RandomForestRegressor() # TODO: vary model types
         model.fit(xtrain, ytrain)
+        models[year] = model
 
         pred_train = model.predict(xtrain)
         r2_train = sklearn.metrics.r2_score(ytrain, pred_train)
@@ -89,3 +99,38 @@ if __name__ == '__main__':
 
     summary = pd.DataFrame(all_summary, columns=['Year', 'Model', 'R2_train', 'R2_test', 'RMSE_train', 'RMSE_test'])
     summary.to_csv(f'{output}/summary.csv', index=False)
+
+    # plot the preds and target by year
+
+    # compare year i to j
+    r2_offyear = np.zeros((len(years), len(years)), dtype=np.float32)
+    rmse_offyear = np.zeros((len(years), len(years)), dtype=np.float32)
+    for i,iyear in enumerate(years):
+        for j,jyear in enumerate(years):
+            pred = models[iyear].predict(x_save_test[jyear])
+            r2_offyear[i,j] = sklearn.metrics.r2_score(y_save_test[jyear], pred)
+            rmse_offyear[i,j] = np.sqrt(sklearn.metrics.mean_squared_error(y_save_test[jyear], pred))
+
+    sns.heatmap(r2_offyear, vmin=0, vmax=1, cbar=True, xticklabels=years, yticklabels=years)
+    #plt.pcolormesh(r2_offyear, vmin=0, vmax=1)
+    #plt.colorbar()
+    #locs, labs = plt.xticks()
+    #plt.xticks(locs, years)
+    #locs, labs = plt.yticks()
+    #plt.yticks(locs, years)
+    plt.title('$R^2$ for predictions across years')
+    plt.savefig(f'{output}/r2_years.png')
+    plt.close()
+
+    sns.heatmap(rmse_offyear, cbar=True, xticklabels=years, yticklabels=years)
+    #plt.pcolormesh(rmse_offyear, vmin=0)
+    #plt.colorbar()
+    #locs, labs = plt.xticks()
+    #plt.xticks(locs, years)
+    #locs, labs = plt.yticks()
+    #plt.yticks(locs, years)
+    plt.title('RMSE for predictions across years')
+    plt.savefig(f'{output}/rmse_years.png')
+
+
+
