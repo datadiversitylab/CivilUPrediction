d8f2c1ea88ee844dee9db7f26609039dc46144f7
Jhan Carlos Salazar Salazar <61802315+salazarjhan95@users.noreply.github.com>
2024-11-26 20:03:27 -0600

    Update NatDisasters.R

--- NatDisasters/NatDisasters.R
+++ NatDisasters/NatDisasters.R
@@ -1,14 +1,15 @@
-setwd("C:/Users/jhanc/Box/Investigacion/Investigaciones/Side projects/Diversity_lab/CivilUnrest/Natural Disasters/Natural Disasters")
+setwd("C:/Users/jhanc/Box/Investigacion/Investigaciones/Side projects/Diversity_lab/CivilUnrest/Natural Disasters")
 
 library(stars)
 library(sf)
 library(units)
 
 # List of the files
-files <- c("gdcyc", "gdcycgdp", "gdcycpro", "gddrg", "gddrggdp", 
-           "gddrgpro", "gdfld", "gdfldgdp", "gdfldmrt", "gdfldpro", 
-           "gdlnd", "gdlndgdp", "gdlndmrt", "gdmhz", "gdmhzgdp")
-
+files <- c("gdcyc", "gdcycpro", "gddrg", "gddrggdp", "gddrgpro", 
+           "gdfld", "gdfldgdp", "gdfldmrt", "gdfldpro", "gdlnd", 
+           "gdlndgdp", "gdlndmrt", "gdmhz", "gdmhzgdp", "gdmhzmrt",
+           "gdpgagdp", "gdpgapro", "gdvol", "gdvolgdp", "gdvolmrt",
+           "gdvolpro")
 
 for (file in files) {
   # Define file paths
@@ -25,19 +26,13 @@ for (file in files) {
   # Reload the .tif
   raster <- read_stars(tif_file)
   
-  # Plot the original raster
-  # plot(raster, downsample = FALSE)
-  
   # Create grid with a resolution of 0.1 x 0.1
   grid <- st_as_stars(st_bbox(raster), dx = 0.1, dy = 0.1)
   
   # Warp (reproject) the raster to the new grid
   raster_reprojected <- st_warp(raster, grid, method = "bilinear", 
                                 use_gdal = TRUE, no_data_value = -9999)
-  
-  # Plot the reprojected raster
-  # plot(raster_reprojected)
-  
+
   # Save the final reprojected raster
   write_stars(raster_reprojected, dsn = final_tif, overwrite = TRUE)
   
